2113597d8a823ed6b6be5a7347e1eb7222138269
blackbeau <ykaiwan@163.com>
2017-08-04 18:14:56 +0800

    Update sethost.ps1

--- sethost.ps1
+++ sethost.ps1
@@ -1,7 +1,16 @@
+Param(
+    [parameter(Mandatory=$true)]
+    [alias("set")]
+    $Settarget,
+    [alias("tar")]
+    $Target,
+    [alias("pa")]
+    $Path)
+
 
 $script:OS=(Get-WmiObject Win32_OperatingSystem).osarchitecture
 
-if($script:OS.Equals("32-bit") -eq 1)
+if($script:OS.Equals("32-bit"))
 {
     Write-Debug "OS: 32bit"
     $script:iniFilePrefix="C:\Program Files\National Instruments\"
@@ -25,12 +34,25 @@ Else
     $script:bashIniFile="/etc/natinst/share/ni-rt.ini"
 }
 Write-Debug ("LabVIEW version: "+$script:labViewVersion.split(" ")[-1])
+(Get-Content  .\setRT.txt ) |  Foreach-Object {
+
+           if($_ -match "^iniFile=")
+           {
+                #Add Lines after the selected pattern
+                "iniFile="+$script:bashIniFile
+           }
+           else
+           {
+           $_
+           }
+  } | Set-Content .\setRT.txt
+
 
 $script:iniFilePath=$script:iniFilePrefix+$script:labViewVersion+"\LabVIEW.ini"
 Write-Debug ("Ini file path: "+$script:iniFilePath)
 
 $script:iniTempFilePath=$script:iniFilePrefix+$script:labViewVersion+"\temp"
-New-Item $script:iniTempFilePath -force -type file
+New-Item $script:iniTempFilePath -force -type file | Out-Null
 
 function GetTokenLine($token)
 {
@@ -48,38 +70,69 @@ function GetTokenLine($token)
 
 function AddIniToken($token,$value)
 {
-    $defaultLineNumberToInsert=(GetTokenLine \[LabVIEW\])
-    $tokenLineNumber =(GetTokenLine $token)
-    echo "$defaultLineNumberToInsert,??,$tokenLineNumber"
-    if($tokenLineNumber -eq 0)
+    $tokenAlreadyExistLineNumber =(GetTokenLine $token)
+    #echo "$defaultLineNumberToInsert,??,$tokenLineNumber"
+    if($tokenAlreadyExistLineNumber -eq 0)
     {
-     $fileArrayList.insert($defaultLineNumberToInsert,$token+"="+$value)
+     $tokenToAdd.add($token+"="+$value)
+     #$fileArrayList.insert($defaultLineNumberToInsert,$token+"="+$value)
     }
     else
     {
-     $fileArrayList[$tokenLineNumber-1]=$token+"="+$value
+     $fileArrayList[$tokenAlreadyExistLineNumber-1] = $token+"="+$value
     }
 }
 
 function CheckINIFileAndUpdate
 {
-    [System.Collections.ArrayList]$fileArrayList=(cat $script:iniFilePath)
+  try
+   {
+    $tokenToAdd = New-Object System.Collections.ArrayList
+    $fileArrayList=[System.Collections.ArrayList](cat $script:iniFilePath -ErrorAction Stop  )
+    $defaultLineNumberToInsert=(GetTokenLine \[LabVIEW\])
     AddIniToken  DWarnDialogMultiples True
     AddIniToken  promoteDWarnInternals True
     AddIniToken  DPrintfLogging True
     AddIniToken  Debugging True
     AddIniToken  numstatusitemstolog 99999
+    $fileArrayList.insert($defaultLineNumberToInsert,$tokenToAdd)
     $fileArrayList | % {$_ | Out-File -append  $script:iniTempFilePath}
-    Write-Debug ("write to temp:"+$?)
-    mv $script:iniTempFilePath $script:iniFilePath -force
-    Write-Debug ("write back :"+$?)
+    Write-Debug ("Write ini to temp file: "+$?)
+    cp $script:iniTempFilePath $script:iniFilePath
+    Write-Debug ("Write temp back to ini file: "+$?)
+    }
+  catch [Exception]
+    {
+    Write-Host $_.Exception.ToString()
+    }
+  finally
+    {
+    Remove-Item $script:iniTempFilePath
+    }
 }
-#(cat $script:iniFilePath | findstr -N \[LabVIEW\])
 
-CheckINIFileAndUpdate
 
+#CheckINIFileAndUpdate
+#./plink -ssh admin@10.144.16.189  -m shc.txt
 #foreach ($line in $fileArrayList)
 #{
 #   #echo $line
 #    $line | Out-File -append  $script:iniTempFilePath
 #}
+
+if($Settarget.Equals("all"))
+{
+    Write-Debug "set both host and target"
+    CheckINIFileAndUpdate
+    ./plink -ssh $target  -m setRT.txt
+}
+elseif($Settarget.Equals("host"))
+{
+    Write-Debug "set only on host"
+    CheckINIFileAndUpdate
+}
+else
+{
+    Write-Debug "set only on target"
+    ./plink -ssh $target  -m setRT.txt
+}
