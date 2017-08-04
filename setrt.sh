6f191cc83fe6b7ac0ba0e6be969229801fecc850
blackbeau <ykaiwan@163.com>
2017-08-04 18:15:54 +0800

    Update setrt.sh

--- setrt.sh
+++ setrt.sh
@@ -1,18 +1,28 @@
 #!/bin/sh
-iniFile=""
+# $1 Ini file path
+iniFile=$1
 iniTempFile=""
-lvrtConfigFile=/etc/init.d/lvrt-wrapper
+lvrtConfigFile="/etc/init.d/lvrt-wrapper"    #  "/etc/init.d/lvrt-wrapper"
+niMaxIniFile="/etc/natinst/share/ni-rt.ini"
+lvtrString="\[LVRT\]"
+sysString="\[SYSTEMSETTINGS\]"
+
 GetTokenLine()
 {
   grep  -n  "$2"   $1  | cut  -d  ":"  -f  1
 }
+
 AddIniToken()
 {
+  # $1 file to check
+  # $2 token
+  # $3 value
+  # $4 ini head to get default line number to insert
   local defaultLineNumberToInsert
   local tokenLineNumber
-  defaultLineNumberToInsert=`GetTokenLine $1 LVRT`
+  defaultLineNumberToInsert=`GetTokenLine $1 "$4"`
   tokenLineNumber=`GetTokenLine $1 $2`
-  #echo $tokenLineNumber $defaultLineNumberToInsert
+  echo $tokenLineNumber,$defaultLineNumberToInsert
   if [ -n "$tokenLineNumber" ]; then
       echo "$2 token exist"
       sed -i "${tokenLineNumber}c\\$2=$3" $1
@@ -21,14 +31,23 @@ AddIniToken()
       sed -i "${defaultLineNumberToInsert}a\\${2}=${3}" $1
   fi
 }
+
 CheckINIFileAndUpdate()
 {
-    AddIniToken $1 DWarnDialogMultiples True
-    AddIniToken $1 promoteDWarnInternals True
-    AddIniToken $1 DPrintfLogging True
-    AddIniToken $1 Debugging True
-    AddIniToken $1 numstatusitemstolog 99999
+    AddIniToken $1 DWarnDialogMultiples True $lvtrString
+    AddIniToken $1 promoteDWarnInternals True $lvtrString
+    AddIniToken $1 DPrintfLogging True $lvtrString
+    AddIniToken $1 Debugging True $lvtrString
+    AddIniToken $1 numstatusitemstolog 99999 $lvtrString
 }
+
+CheckNIMaxINIFileAndUpdate()
+{
+    AddIniToken $1 SafeMode.enabled False $sysString
+    AddIniToken $1 sshd.enabled True $sysString
+    AddIniToken $1 ConsoleOut.enabled True $sysString
+}
+
 IncreaseThreadSizeAndDumpFileSize()
 {
   local defaultLineNumberToInsert
@@ -44,10 +63,9 @@ IncreaseThreadSizeAndDumpFileSize()
       sed -i "${defaultLineNumberToInsert}a\\${2} ${3}" $1
   fi
 }
-#CheckINIFileAndUpdate $1
-#
-# #/etc/init.d/lvrt-wrapper
-IncreaseThreadSizeAndDumpFileSize $1 "ulimit -s" 512
-IncreaseThreadSizeAndDumpFileSize $1 "ulimit -c" unlimited
 
-rm core_dump.\!usr\!local\!natinst\!labview\!lvrt
+CheckINIFileAndUpdate $iniFile
+CheckNIMaxINIFileAndUpdate $niMaxIniFile
+IncreaseThreadSizeAndDumpFileSize $lvrtConfigFile "ulimit -s" 512
+IncreaseThreadSizeAndDumpFileSize $lvrtConfigFile "ulimit -c" unlimited
+reboot
