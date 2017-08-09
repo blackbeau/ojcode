416f527e1822c8395da52dd018ca8afd6aa57580
blackbeau <ykaiwan@163.com>
2017-08-09 16:37:38 +0800

    Update sethost.ps1

--- sethost.ps1
+++ sethost.ps1
@@ -1,13 +1,53 @@
+<#
+
+.SYNOPSIS
+This is a simple Powershell script to explain how to create help
+
+.DESCRIPTION
+The script itself will only print 'Hello World'. But that's cool. It's main objective is to show off the cool help thingy anyway.
+
+.EXAMPLE
+./HelloWorld.ps1
+
+.NOTES
+Put some notes here.
+
+.LINK
+http://kevinpelgrims.wordpress.com
+
+#>
+
+
+#Param(
+ #   [parameter(Mandatory=$true)] #(Mandatory=$true)
+  #  [alias("set")]
+  #  $Settarget,
+  #  [alias("tar")]
+  #  $Target,
+  #  [alias("pa")]
+  #  $Path)
+  
 Param(
-    [parameter(Mandatory=$true)]
-    [alias("set")]
-    $Settarget,
-    [alias("tar")]
-    $Target,
-    [alias("pa")]
-    $Path)
-
-
+    [string]
+    [alias("enable")]
+    $EnableDebugSetting,
+    [string]
+    [alias("disable")]
+    $DisableDebugSetting,
+    [string]
+    [alias("fetch")]
+    $FetchCoreDump,
+    [string]
+    [alias("ssh")]
+    $SSHAdress,
+    [string]
+    $Path
+  )
+    
+    
+$script:scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$script:setRTPath = $script:scriptPath+"/setRT.txt"
+echo $script:scriptPath
 $script:OS=(Get-WmiObject Win32_OperatingSystem).osarchitecture
 
 if($script:OS.Equals("32-bit"))
@@ -21,7 +61,7 @@ Else
     $script:iniFilePrefix="C:\Program Files (x86)\National Instruments\"
 }
 
-$script:labViewVersion=(Get-ChildItem  $iniFile | Select-Object -Property Name | findstr LabVIEW)[-1].TrimEnd()
+$script:labViewVersion=(Get-ChildItem  $script:iniFilePrefix | Select-Object -Property Name | findstr LabVIEW)[-1].TrimEnd()
 
 if($script:labViewVersion.split(" ")[-1].CompareTo("2014") -eq 1)
 {
@@ -34,8 +74,8 @@ Else
     $script:bashIniFile="/etc/natinst/share/ni-rt.ini"
 }
 Write-Debug ("LabVIEW version: "+$script:labViewVersion.split(" ")[-1])
-(Get-Content  .\setRT.txt ) |  Foreach-Object {
-
+(Get-Content  $script:setRTPath  ) |  Foreach-Object { 
+           
            if($_ -match "^iniFile=")
            {
                 #Add Lines after the selected pattern
@@ -45,7 +85,7 @@ Write-Debug ("LabVIEW version: "+$script:labViewVersion.split(" ")[-1])
            {
            $_
            }
-  } | Set-Content .\setRT.txt
+  } | Set-Content $script:setRTPath 
 
 
 $script:iniFilePath=$script:iniFilePrefix+$script:labViewVersion+"\LabVIEW.ini"
@@ -61,11 +101,11 @@ function GetTokenLine($token)
    {
       return 0
    }
-   else
+   else 
    {
       return $findAns.split(":")[0]
    }
-
+   
 }
 
 function AddIniToken($token,$value)
@@ -83,7 +123,7 @@ function AddIniToken($token,$value)
     }
 }
 
-function CheckINIFileAndUpdate
+function CheckINIFileAndUpdate()
 {
   try
    {
@@ -98,20 +138,116 @@ function CheckINIFileAndUpdate
     $fileArrayList.insert($defaultLineNumberToInsert,$tokenToAdd)
     $fileArrayList | % {$_ | Out-File -append  $script:iniTempFilePath}
     Write-Debug ("Write ini to temp file: "+$?)
-    cp $script:iniTempFilePath $script:iniFilePath
+    cp $script:iniTempFilePath $script:iniFilePath 
     Write-Debug ("Write temp back to ini file: "+$?)
     }
-  catch [Exception]
-    {
-    Write-Host $_.Exception.ToString()
+  catch [Exception] 
+    { 
+    Write-Host $_.Exception.ToString() 
     }
-  finally
+  finally 
     {
     Remove-Item $script:iniTempFilePath
     }
 }
 
+function UndoINIFileAndUpdate
+{
+  try
+   {
+    Get-Content $script:iniFilePath -ErrorAction Stop |  Foreach-Object { 
+           
+           if($_ -match "^(?!((^DWarnDialogMultiples)|(^promoteDWarnInternals)|(^DPrintfLogging)|(^Debugging)|(^numstatusitemstolog)))")
+           {
+               $_     
+           } 
+         } | Set-Content  $script:iniTempFilePath -ErrorAction Stop
+    cp $script:iniTempFilePath $script:iniFilePath -ErrorAction Stop
+    Write-Debug ("Write temp back to ini file: "+$?)
+    }
+  catch [Exception] 
+    { 
+    Write-Host $_.Exception.ToString() 
+    }
+  finally 
+    {
+    Remove-Item $script:iniTempFilePath
+    }
+}
 
+#UndoINIFileAndUpdate
+
+function AddIniTokenToTempFile($token,$value,$isinsert)
+{
+$isTokenFind=$FALSE
+if($isinsert){$matchrex="\[LabVIEW\]"}
+else {$matchrex=$token}
+( Get-Content  $script:iniTempFilePath  -ErrorAction Stop ) |  Foreach-Object {           
+           if($_ -match "^"+$matchrex)
+           {
+                #Add Lines after the selected pattern
+                if($isinsert){$_}
+                $token+"="+$value
+                $isTokenFind=$TRUE
+           }
+           else
+           {
+                $_
+           }
+  } | Set-Content $script:iniTempFilePath  -ErrorAction Stop
+
+ return $isTokenFind
+}
+
+function AddOneToken($token,$value)
+{
+    #$re =  AddIniTokenToTempFile $token $value $false
+    if( -not  (AddIniTokenToTempFile $token $value $false)  )
+    {
+     AddIniTokenToTempFile $token $value $true >$null     
+    }
+}
+
+function CheckINIFileAndUpdateV2($isUndo)
+{
+ try
+   {
+    if($isUndo)
+    {
+     $iniTokenFlag="False"
+     $iniTokenNum="0"
+    }
+    else 
+    {
+     $iniTokenFlag="True"
+     $iniTokenNum="99999"
+    }
+    Get-Content  $script:iniFilePath -ErrorAction Stop | Set-Content $script:iniTempFilePath -ErrorAction Stop
+    
+    AddOneToken  DWarnDialogMultiples $iniTokenFlag 
+    
+    AddOneToken  promoteDWarnInternals $iniTokenFlag 
+    
+    AddOneToken  DPrintfLogging $iniTokenFlag 
+   
+    AddOneToken  Debugging $iniTokenFlag 
+ 
+    AddOneToken  numstatusitemstolog $iniTokenNum
+    
+    cp $script:iniTempFilePath $script:iniFilePath -ErrorAction Stop
+    Write-Debug ("Write temp back to ini file: "+$?)
+   }
+ catch [Exception] 
+   { 
+    Write-Host $_.Exception.ToString() 
+   }
+ finally 
+   {
+    Remove-Item $script:iniTempFilePath
+   }
+}
+
+#CheckINIFileAndUpdateV2 $false
 #CheckINIFileAndUpdate
 #./plink -ssh admin@10.144.16.189  -m shc.txt
 #foreach ($line in $fileArrayList)
@@ -120,19 +256,20 @@ function CheckINIFileAndUpdate
 #    $line | Out-File -append  $script:iniTempFilePath
 #}
 
-if($Settarget.Equals("all"))
-{
-    Write-Debug "set both host and target"
-    CheckINIFileAndUpdate
-    ./plink -ssh $target  -m setRT.txt
-}
-elseif($Settarget.Equals("host"))
-{
-    Write-Debug "set only on host"
-    CheckINIFileAndUpdate
-}
-else
-{
-    Write-Debug "set only on target"
-    ./plink -ssh $target  -m setRT.txt
-}
+#if($Settarget.Equals("all"))
+#{
+#    Write-Debug "set both host and target"
+#    CheckINIFileAndUpdate
+#    ./plink -ssh $target  -m setRT.txt
+#}
+#elseif($Settarget.Equals("host"))
+#{
+ #   Write-Debug "set only on host"
+ #   CheckINIFileAndUpdate
+#}
+#else
+#{
+#    Write-Debug "set only on target"
+#    ./plink -ssh $target  -m setRT.txt
+#}
+
