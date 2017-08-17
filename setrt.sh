336675706b7686e5976398669582f18192c93ae1
blackbeau <ykaiwan@163.com>
2017-08-17 13:47:47 +0800

    Update setrt.sh

--- setrt.sh
+++ setrt.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # $1 Ini file path
 iniFile=$1
 isUndoOperation=true
@@ -7,7 +7,16 @@ lvrtConfigFile="/etc/init.d/lvrt-wrapper"    #  "/etc/init.d/lvrt-wrapper"
 niMaxIniFile="/etc/natinst/share/ni-rt.ini"
 lvtrString="\[LVRT\]"
 sysString="\[SYSTEMSETTINGS\]"
-
+declare -A constEnableTokenTable=( [DWarnDialogMultiples]=True
+                             [promoteDWarnInternals]=True
+                             [DPrintfLogging]=True
+                             [Debugging]=True
+                             [numstatusitemstolog]=99999 )
+declare -A constDisableTokenTable=( [DWarnDialogMultiples]=False
+                            [promoteDWarnInternals]=False
+                            [DPrintfLogging]=False
+                            [Debugging]=False
+                            [numstatusitemstolog]=1000 )
 GetTokenLine()
 {
   grep  -n  "$2"   $1  | cut  -d  ":"  -f  1
@@ -33,21 +42,37 @@ AddIniToken()
   fi
 }
 
-CheckINIFileAndUpdate()
+EnableINITokens()
 {
-    AddIniToken $1 DWarnDialogMultiples True $lvtrString
-    AddIniToken $1 promoteDWarnInternals True $lvtrString
-    AddIniToken $1 DPrintfLogging True $lvtrString
-    AddIniToken $1 Debugging True $lvtrString
-    AddIniToken $1 numstatusitemstolog 99999 $lvtrString
+  if [ -n "$1" ]; then
+    AddIniToken $iniFile $1 ${constEnableTokenTable[$1]} $lvtrString
+  else
+      for key in "${!constEnableTokenTable[@]}"
+      do
+        AddIniToken $iniFile $key ${constEnableTokenTable[$key]} $lvtrString
+      done
+  fi
+    # AddIniToken $1 DWarnDialogMultiples True $lvtrString
+    # AddIniToken $1 promoteDWarnInternals True $lvtrString
+    # AddIniToken $1 DPrintfLogging True $lvtrString
+    # AddIniToken $1 Debugging True $lvtrString
+    # AddIniToken $1 numstatusitemstolog 99999 $lvtrString
 }
-UndoINIFile()
+DisableINITokens()
 {
-  AddIniToken $1 DWarnDialogMultiples False $lvtrString
-  AddIniToken $1 promoteDWarnInternals False $lvtrString
-  AddIniToken $1 DPrintfLogging False $lvtrString
-  AddIniToken $1 Debugging False $lvtrString
-  AddIniToken $1 numstatusitemstolog 0 $lvtrString
+  if [ -n "$1" ]; then
+    AddIniToken $iniFile $1 ${constDisableTokenTable[$1]} $lvtrString
+  else
+      for key in "${!constDisableTokenTable[@]}"
+      do
+        AddIniToken $iniFile $key ${constDisableTokenTable[$key]} $lvtrString
+      done
+  fi
+  # AddIniToken $1 DWarnDialogMultiples False $lvtrString
+  # AddIniToken $1 promoteDWarnInternals False $lvtrString
+  # AddIniToken $1 DPrintfLogging False $lvtrString
+  # AddIniToken $1 Debugging False $lvtrString
+  # AddIniToken $1 numstatusitemstolog 0 $lvtrString
 }
 CheckNIMaxINIFileAndUpdate()
 {
@@ -58,6 +83,7 @@ CheckNIMaxINIFileAndUpdate()
 
 SetThreadSizeAndDumpFileSize()
 {
+  echo $1
   local defaultLineNumberToInsert
   local tokenLineNumber
   defaultLineNumberToInsert=`GetTokenLine $1 "$2"`
@@ -74,30 +100,45 @@ SetThreadSizeAndDumpFileSize()
 
 IncreaseThreadSizeAndDumpFileSize()
 {
-  setThreadSizeAndDumpFileSize $lvrtConfigFile "ulimit -s" 512
-  setThreadSizeAndDumpFileSize $lvrtConfigFile "ulimit -c" unlimited
+  if [ "$1" = "Stack" ];then
+      SetThreadSizeAndDumpFileSize $lvrtConfigFile "ulimit -s" $2
+  elif [ "$1" = "CoreDump" ];then
+      SetThreadSizeAndDumpFileSize $lvrtConfigFile "ulimit -c" $2
+  else
+    SetThreadSizeAndDumpFileSize $lvrtConfigFile "ulimit -s" 512
+    SetThreadSizeAndDumpFileSize $lvrtConfigFile "ulimit -c" unlimited
+  fi
 }
+
 UndoIncreaseThreadSizeAndDumpFileSize()
 {
-  setThreadSizeAndDumpFileSize $lvrtConfigFile "ulimit -s" 256
-  setThreadSizeAndDumpFileSize $lvrtConfigFile "ulimit -c" 4096
+  if [ "$1" = "Stack" ];then
+      SetThreadSizeAndDumpFileSize $lvrtConfigFile "ulimit -s" $2
+  elif [ "$1" = "CoreDump" ];then
+      SetThreadSizeAndDumpFileSize $lvrtConfigFile "ulimit -c" $2
+  else
+    SetThreadSizeAndDumpFileSize $lvrtConfigFile "ulimit -s" 256
+    SetThreadSizeAndDumpFileSize $lvrtConfigFile "ulimit -c" 4096
+  fi
 }
 undoAllOperation(){
   UndoINIFile
   UndoIncreaseThreadSizeAndDumpFileSize
 }
 
-if [ $isUndoOperation = "false" ];then
-  echo "false"
-  CheckINIFileAndUpdate $iniFile
-  CheckNIMaxINIFileAndUpdate $niMaxIniFile
-  IncreaseThreadSizeAndDumpFileSize
-  reboot
-else
-  echo "true"
-  undoAllOperation
-fi
-# CheckINIFileAndUpdate $iniFile
+# if [ $isUndoOperation = "false" ];then
+#   echo "false"
+#   EnableINITokens $iniFile
+#   CheckNIMaxINIFileAndUpdate $niMaxIniFile
+#   IncreaseThreadSizeAndDumpFileSize
+#   reboot
+# else
+#   echo "true"
+#   undoAllOperation
+# fi
+iniFilePath=$1
+IncreaseThreadSizeAndDumpFileSize
+#DisableINITokens numstatusitemstolog
 # CheckNIMaxINIFileAndUpdate $niMaxIniFile
 # IncreaseThreadSizeAndDumpFileSize
 # reboot
