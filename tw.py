d3541cbca25f2ef349b7fb5a88a78a6093cca046
blackbeau <ykaiwan@163.com>
2017-12-28 00:24:30 +0800

    Update tw.py

--- tw.py
+++ tw.py
@@ -1,17 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Dec 25 15:16:09 2017
+Spyder Editor
 
+This is a temporary script file.
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 25 15:16:09 2017
 @author: KaiYang
 """
 
 # -*- coding: utf-8 -*-
 """
 Created on Wed Dec 20 17:30:40 2017
-
 @author: KaiYang
 """
 import copy
+import os
 class PyLuaTblParser:
     def __init__(self):
         self._indict={}
@@ -23,14 +29,14 @@ class PyLuaTblParser:
             self._indict=self._myparse()
         else:
             raise NameError('not dict')
-        return self._indict
+#        return self._indict
     def _myparse(self):
         re = {'islist':True,'data':{}}
         inner_id=1
         last_tk=None
         tk=self._gettoken()
-        while tk:
-            print tk,self.at,self.ch,last_tk
+        while tk[1]:
+            #print tk,self.at,self.ch,last_tk
             if tk[0] == 'token':
                 if tk[1] == '{':
                     re['data'][inner_id]=self._myparse()
@@ -196,14 +202,11 @@ class PyLuaTblParser:
                 if obj['data'][k] != None:
                     re[k]=self._myDumpDict(obj['data'][k])
         return re              
-        
-        
-        
-            
-        
-test_str = """{\nroot = {\n\t"Test Pattern String",\n\t-- {"object with 1 member" = {"array with 1 element",},},\n\t{["object wh 1 member"] = {"array with 1 element",},},\n\t{},\n\t[99] = -42,\n\t[98] = {{}},\n\t[97] = {{},{}},\n\t[96] = {{}, 1, 2, nil},\n\t[95] = {1, 2, {["1"] = 1}},\n\t[94] = { {["1"]=1, ["2"]=2}, {1, ["2"]=2}, ["3"] = 3 },\n\ttrue,\n\tfalse,\n\tnil,\n\t{\n\t\t["integer"]= 1234567890,\n\t\treal=-9876.543210,\n\t\te= 0.123456789e-12,\n\t\tE= 1.234567890E+34,\n\t\tzero = 0,\n\t\tone = 1,\n\t\tspace = " ",\n\t\tquote = "\\"",\n\t\tbackslash = "\\\\",\n\t\tcontrols = "\\b\\f\\n\\r\\t",\n\t\tslash = "/ & \\\\",\n\t\talpha= "abcdefghijklmnopqrstuvwyz",\n\t\tALPHA = "ABCDEFGHIJKLMNOPQRSTUVWYZ",\n\t\tdigit = "0123456789",\n\t\tspecial = "`1~!@#$%^&*()_+-={\':[,]}|;.</>?",\n\t\thex = "0x01230x45670x89AB0xCDEF0xabcd0xef4A",\n\t\t["true"] = true,\n\t\t["false"] = false,\n\t\t["nil"] = nil,\n\t\tarray = {nil, nil,},\n\t\tobject = {  },\n\t\taddress = "50 St. James Street",\n\t\turl = "http://www.JSON.org/",\n\t\tcomment = "// /* <!-- --",\n\t\t["# -- --> */"] = " ",\n\t\t[" s p a c e d " ] = {1,2 , 3\n\n\t\t\t,\n\n\t\t\t4 , 5        ,          6           ,7        },\n\t\t--[[[][][]  Test multi-line comments\n\t\t\tcompact = {1,2,3,4,5,6,7},\n\t- -[luatext = "{\\"object with 1 member\\" = {\\"array with 1 element\\"}}",\n\t\tquotes = "&#34; (0x0022) %22 0x22 034 &#x22;",\n\t\t["\\\\\\"\\b\\f\\n\\r\\t`1~!@#$%^&*()_+-=[]{}|;:\',./<>?"]\n\t\t= "A key can be any string"]]\n\t--         ]]\n\t\tcompact = {1,2,3,4,5,6,7},\n\t\tluatext = "{\\"object with 1 member\\" = {\\"array with 1 element\\"}}",\n\t\tquotes = "&#34; (0x0022) %22 0x22 034 &#x22;",\n\t\t["\\\\\\"\\b\\f\\n\\r\\t`1~!@#$%^&*()_+-=[]{}|;:\',./<>?"]\n\t\t= "A key can be any string"\n\t},\n\t0.5 ,31415926535897932384626433832795028841971693993751058209749445923.\n\t,\n\t3.1415926535897932384626433832795028841971693993751058209749445923\n\t,\n\n\t1066\n\n\n\t,"rosebud"\n\n}}"""
-#test_str='{array = {65,23,5,},dict = {mixed = {43,54.33,false,9,string = "value",},array = {3,6,4,},string = "value",},}'
-#test_str=r'{nil}'
-myp=PyLuaTblParser()
-data=myp.load(test_str)
-du=myp.dump()
+    def loadLuaTable(self,f):
+        with open(f) as fi:
+             self.load(fi.read())
+    def dumpLuaTable(self, f):
+#        if os.path.exists(f):
+#            raise NameError('file exist')
+        with open(f, 'w') as fi:
+            fi.write(self.dump())
